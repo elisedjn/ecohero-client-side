ca85ed7ccb55878d9f3e51c8a24ee16e35acdc8c
Elise Djn <elisedjn@protonmail.com>
2020-08-29 18:25:13 +0200

    leaderboard style

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
-import {API_URL} from '../config'
-import "./styles/Leaderboard.css"
-
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../config";
+import "./styles/Leaderboard.css";
 
 class Leaderboard extends Component {
+  state = {
+    users: [],
+  };
 
-    state={
-        users:[]
+  componentDidMount() {
+    axios.get(`${API_URL}/users/leaderboard`).then((res) => {
+      this.setState({
+        users: res.data,
+      });
+    });
+  }
+
+  render() {
+    if (!this.state.users) {
+      return <p>Loading...</p>;
     }
 
-    componentDidMount(){
-        axios.get(`${API_URL}/users/leaderboard`)
-            .then((res) => {
-                this.setState({
-                    users: res.data
-                })
-            })
-    }
-
-
-    render() {
-
-        if (!this.state.users){
-            return <p>Loading...</p>
-        }
-
-        return (
-            <div className="leaderboard">
-                <h3 className="HallTitle">Hall of Heroes</h3>
-                {
-                    this.state.users.map((user) => {
-                        const {username, points, _id} = user
-                        return <Link to={`/user/${_id}`}><div className="container">
-                                                            <ol className="list">
-                                                            <li className="listItems">{username} {points} points</li>
-                                                            </ol>
-                                                         </div></Link>
-                    })
-                }
-            </div>
-        )
-    }
+    return (
+      <div className="leaderboard">
+        <h3 className="HallTitle">Hall of Heroes</h3>
+        <ol className="list">
+          {this.state.users.map((user) => {
+            const { username, points, _id } = user;
+            return (
+              <Link to={`/user/${_id}`}>
+              <div className="listItems">
+                  <li >
+                    {username} {points} points
+                  </li>
+                  </div>
+              </Link>
+            );
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
-
-
-export default Leaderboard
+export default Leaderboard;
