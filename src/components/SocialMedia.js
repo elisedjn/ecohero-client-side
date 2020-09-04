b20465776a5eebd9b581c23e328f2c2d7fa28e24
Elise Djn <elisedjn@protonmail.com>
2020-09-04 12:11:59 +0200

    stupid s

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -15,15 +15,15 @@ export default function SocialMedia(props) {
         
       <div id="socialMedia" >
       <div className="sharing-logo">
-        <FacebookShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievements/${props.achievementID}`} quote={"Hey! I have achieved this challenge at Eco-Hero! Check it out!"}><FacebookIcon size={30} /></FacebookShareButton>
+        <FacebookShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievement/${props.achievementID}`} quote={"Hey! I have achieved this challenge at Eco-Hero! Check it out!"}><FacebookIcon size={30} /></FacebookShareButton>
       </div>
 
       <div className="sharing-logo">
-        <TwitterShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievements/${props.achievementID}`} title={"My new Success in EcoHero"}><TwitterIcon size={30} /></TwitterShareButton>
+        <TwitterShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievement/${props.achievementID}`} title={"My new Success in EcoHero"}><TwitterIcon size={30} /></TwitterShareButton>
       </div>
 
       <div className="sharing-logo">
-        <WhatsappShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievements/${props.achievementID}`} title={"Hey! I have achieved this challenge at Eco-Hero! Check it out!"}><WhatsappIcon size={30} /></WhatsappShareButton> 
+        <WhatsappShareButton className="sharing-logo" url={`https://eco-hero.herokuapp.com/achievement/${props.achievementID}`} title={"Hey! I have achieved this challenge at Eco-Hero! Check it out!"}><WhatsappIcon size={30} /></WhatsappShareButton> 
       </div>
     </div>
             
